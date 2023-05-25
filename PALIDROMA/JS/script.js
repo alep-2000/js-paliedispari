9073eb423df05788ccf2483d29ebb56fa9c47069
alep-2000 <alessiopiras056@gmail.com>
2023-05-25 17:50:16 +0200

    [FEAT]Add palidroma exercise finisched

--- PALIDROMA/JS/script.js
+++ PALIDROMA/JS/script.js
@@ -1,1 +1,29 @@
 "use strict"
+
+// CHIEDERE ALL'UTENTE DI INSERIRE UNA PAROLA
+
+const word= prompt('Inserisci una parola');
+
+// UTILIZZARE IL METODO SPLIT
+let word_space= word.split('');
+console.log(word_space);
+
+//UTILIZZARE METODO REVERSE
+let word_reverse= word_space.reverse();
+console.log(word_reverse)
+
+//UTILIZZARE METODO JOIN
+let word_join= word_reverse.join('') 
+console.log(word_join);
+
+function palindrom (word){
+    console.log(word);
+    if(word_join === word){
+        console.log("E' PALINDROMA");
+    }
+    else{
+        console.log("NON E' PALINDROMA");
+    }
+}
+
+palindrom(word);
